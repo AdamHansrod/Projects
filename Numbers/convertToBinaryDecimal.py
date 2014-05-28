ed583dfe52c330ed6d57892f49bab655c0d9b95c
AdamHansrod <AdamHansrod@gmail.com>
2014-05-28 13:16:34 +0100

    separate out function for binary columns

--- Numbers/convertToBinaryDecimal.py
+++ Numbers/convertToBinaryDecimal.py
@@ -1,7 +1,11 @@
 def convertToBinary(decimalNumber,numberOfBits):
+def getListOfBinaryColumns(numberOfBits):
     columns = []
     for column in range(0,numberOfBits):
         columns.append(2**column)
+    return columns
+def convertToBinary(decimalNumber,numberOfBits):
+    columns = getListOfBinaryColumns(numberOfBits)
     binaryNumber = []
     for column in reversed(columns):##get out columns in the right order
         count = 0
