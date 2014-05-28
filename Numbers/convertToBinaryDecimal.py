c15991e33c99efb73ebd2361a2e33b13cc23917a
AdamHansrod <AdamHansrod@gmail.com>
2014-05-28 12:41:32 +0100

    convertToBinary

---
+++ Numbers/convertToBinaryDecimal.py
@@ -0,0 +1,22 @@
+def convertToBinary(decimalNumber,numberOfBits):
+    columns = []
+    for column in range(0,numberOfBits):
+        columns.append(2**column)
+    binaryNumber = []
+    for column in reversed(columns):##get out columns in the right order
+        count = 0
+        while(True):
+            if (decimalNumber - column < 0):
+                break
+            else:
+                decimalNumber = decimalNumber - column 
+                count = count + 1
+        binaryNumber.append(count)
+    return ''.join(map(str, binaryNumber)) ##return a string
+
+    
+
+binary = convertToBinary(255,8)
+print binary
+
+
